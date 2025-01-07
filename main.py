1ff6505f695aaf31e5cc943640720c2c04bfb1b3
kylew2001 <kswalker2201@gmail.com>
2025-01-07 17:46:56 +1300

    commit

---
+++ main.py
@@ -0,0 +1,12 @@
+name = "Kyle"
+
+print(len(name)) #Length
+print(name.find("y")) #Find character in a string
+print(name.capitalize()) #Capitalise first letter of string
+print(name.upper()) #all caps
+print(name.lower()) #all lower case
+print(name.isdigit()) #is number?
+print(name.isalpha()) #contains only letters?
+print(name.count("e")) #count of specific letter
+print(name.replace("e","ie")) #Change letters
+print(name*3) #Print multiple times
